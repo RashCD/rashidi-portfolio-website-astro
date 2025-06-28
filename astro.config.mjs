e4d585395fd75b149bb0a2abf12c1f9a456faff5
RashCD <mrrash91@gmail.com>
2025-06-29 01:57:27 +0800

    upgrade astro to v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false,
-  }), react()]
+	site: "https://example.com",
+	server: {
+		port: 3000,
+	},
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
 });
